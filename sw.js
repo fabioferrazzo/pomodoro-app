54fad6d5b902fb636f48bed4708a81d1da211204
fabioferrazzo <fiscal10.hteixeira@gmail.com>
2026-04-13 11:26:48 -0300

    atualizacao sw v3

--- sw.js
+++ sw.js
@@ -1,24 +1,11 @@
-// Força o Service Worker a se atualizar imediatamente
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
 
-// Garante que o SW assuma o controle das abas abertas
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
-
-// Escuta o clique na notificação do Windows/Android
 self.addEventListener('notificationclick', (e) => {
-  e.notification.close(); // Fecha o banner da notificação
-  
-  // Tenta focar na janela do App que já está aberta ou abre uma nova
+  e.notification.close();
   e.waitUntil(
     clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
-      if (clientList.length > 0) {
-        let client = clientList[0];
-        if (client.focus) return client.focus();
-      }
+      if (clientList.length > 0) return clientList[0].focus();
       return clients.openWindow('./');
     })
   );
